f0169097c2ac0b10ad52f65c05fc056c40b2fa2b
Jonathas Miante <jonathasme@gmail.com>
2026-01-21 17:46:08 -0300

    Tipos de Dados - Strings

---
+++ 02 - JS para Iniciantes/tipo_de_dados/script.js
@@ -0,0 +1,50 @@
+var nome = 'André'; // String
+var idade = 28; // Number
+var possuiFaculdade = true; // Boolean
+var time; // Undefined
+var comida = null; // Null
+var simbolo = Symbol() // Symbol
+var novoObjeto = {} // Object
+
+
+var nome = 'André';
+console.log(typeof nome);
+// retorna string
+
+
+// STRINGS
+var nome = 'André';
+var sobrenome = 'Rafael';
+var nomeCompleto = nome + ' ' + sobrenome;
+
+var gols = 1000;
+var frase = 'Romário fez ' + gols + ' gols';
+
+
+'JavaScript é "super" fácil';
+"JavaScript é 'super' fácil";
+"JavaScript é \"super\" fácil";
+`JavaScript é "super" fácil"`;
+// "JavaScript é "super" fácil"; // Inválido
+
+// Template String
+var gols = 1000;
+var frase1 = 'Romário fez ' + gols + ' gols';
+var frase2 = `Romário fez ${gols} gols`; // Utilizando Template String
+
+
+
+// Declare uma variável contendo uma string
+nome = 'Jonathas';
+
+// Declare uma variável contendo um número dentro de uma string
+
+
+// Declare uma variável com a sua idade
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+// Coloque a seguinte frase em uma variável: It's time
+
+// Verifique o tipo da variável que contém o seu nome
