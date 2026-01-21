fe6ae4e07b76fba624fe6c849ed5fb01be726708
Jonathas Miante <jonathasme@gmail.com>
2026-01-21 17:40:07 -0300

    Variáveis

---
+++ variaveis/script.js
@@ -0,0 +1,62 @@
+// var => Pode Alterar e Redeclarar
+// let  => Pode Alterar e não pode Redeclarar
+// const = > Não pode Alterar ou Redeclarar
+
+var nome = 'Jonathas';
+var idade = 36;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+
+
+var sobrenome = 'Henrique', cidade='Rio';
+console.log(sobrenome, cidade)
+
+
+var semDefinir;
+console.log(semDefinir); // undefined
+
+/*
+Comentário multiplas linhas
+*/
+
+console.log(comida);
+var comida='Arroz';
+console.log(comida);
+
+
+var time = 'Vasco';
+console.log(time);
+
+time = 'Palmeiras';
+console.log(time);
+
+
+
+console.log('##### EXERCÍCIOS #####')
+
+// Declarar uma variável com o seu nome
+var nome = 'Jonathas'
+
+// Declarar uma variável com a sua idade
+var idade = 36;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comida;
+
+// Atribuir valor a sua comida favorita
+comida = 'Sorvete';
+
+console.log(nome, idade, comida);
+
+// Declarar 5 variáveis diferentes sem valores
+var a=1, b=2, c=10, d=15, e=30;
+console.log(a, b, c, d, e)
